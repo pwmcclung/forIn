35f0e982926b147a3f9c685b68ce6ec2932e2d7e
Patrick <78838481+pwmcclung@users.noreply.github.com>
2022-05-01 08:30:02 -0600

    Create forIn.js

---
+++ forIn.js
@@ -0,0 +1,13 @@
+function giveMeFive(obj){
+  let five = []
+  for (let key in obj){
+
+   if (key.length === 5){
+     
+     five.push(key)
+    }
+    if (obj[key].length === 5){
+      five.push(obj[key])
+    }
+    
+  }
